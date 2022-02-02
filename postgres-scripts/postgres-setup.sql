9515c7590cdde498c379e2eda6e48be427b01f97
Evan Rittenhouse <evan.rittenhouse@uscis.dhs.gov>
2022-02-02 13:24:07 -0500

    Working Postgres, need cxn pool next

--- postgres-scripts/postgres-setup.sql
+++ postgres-scripts/postgres-setup.sql
@@ -1,13 +1,13 @@
-create table user (
-    id integer primary key,
+create table todo_user (
+    id serial primary key,
     first_name varchar(128) not null,
-    last_name varchar(128) not null,
+    last_name varchar(128) not null
 );
 
 create table todo_item (
-    id integer primary key,
+    id serial primary key,
     user_id integer not null,
     item_desc text not null,
 
-    constraint todo_item_user_id_fk foreign key(user_id) references user(id),
+    constraint todo_item_user_id_fk foreign key(user_id) references todo_user(id)
 );
