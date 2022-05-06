3a008ee6845499bc6b328731849cd76ba3525f60
Evan Rittenhouse <evan.rittenhouse@uscis.dhs.gov>
2022-05-06 17:05:30 -0400

    Switch to Rust 2021, rustfmt everything

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 use std::env;
 
 use actix_web::*;
-use log::*;
 use dotenv::dotenv;
+use log::*;
 
-
-mod routes;
 mod db;
+mod routes;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -15,7 +14,7 @@ async fn main() -> std::io::Result<()> {
         .filter_level(LevelFilter::Info)
         .filter_module("sqlx", LevelFilter::Warn)
         .init();
-    let db_url = env::var("DATABASE_URL").expect("Could not get database URL from environment");    
+    let db_url = env::var("DATABASE_URL").expect("Could not get database URL from environment");
 
     let sqlx_db_connection = db::connect_sqlx(db_url.as_str()).await;
 
@@ -28,9 +27,8 @@ async fn main() -> std::io::Result<()> {
             .route("/hey", web::get().to(routes::manual_hello))
             .configure(routes::add_user_routes)
             .configure(routes::add_task_routes)
-
     })
-        .bind("0.0.0.0:8080")?
-        .run()
-        .await;
+    .bind("0.0.0.0:8080")?
+    .run()
+    .await;
 }
--- src/routes.rs
+++ src/routes.rs
@@ -1,6 +1,6 @@
 use actix_web::*;
 use log::*;
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 use sqlx::PgPool;
 
 use crate::db::{self, DbError};
@@ -22,7 +22,6 @@ pub async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
-
 pub fn add_user_routes(config: &mut web::ServiceConfig) {
     config
         .route("/users", web::get().to(get_users))
@@ -38,11 +37,14 @@ pub async fn get_users(pg_pool: web::Data<PgPool>) -> impl Responder {
         Err(db_err) => {
             error!("User retrieve failure: {}", db_err);
             HttpResponse::InternalServerError().json("Failed to retrieve users")
-        },
+        }
     }
 }
 
-pub async fn create_user(pg_pool: web::Data<PgPool>, user_to_create: web::Json<db::NewUser>) -> impl Responder {
+pub async fn create_user(
+    pg_pool: web::Data<PgPool>,
+    user_to_create: web::Json<db::NewUser>,
+) -> impl Responder {
     info!("Attempt to create user: {}", user_to_create);
     let db_cxn = pg_pool.get_ref();
     let creation_result = db::create_user(db_cxn, &user_to_create.into_inner()).await;
@@ -57,7 +59,8 @@ pub async fn create_user(pg_pool: web::Data<PgPool>, user_to_create: web::Json<d
 
 // Testing adding a "controller" to the app
 pub fn add_task_routes(config: &mut web::ServiceConfig) {
-    config.service(get_tasks_for_user)
+    config
+        .service(get_tasks_for_user)
         .service(get_task_for_user)
         .service(add_task_for_user)
         .service(update_task_for_user)
@@ -65,7 +68,10 @@ pub fn add_task_routes(config: &mut web::ServiceConfig) {
 }
 
 #[get("/users/{user_id}/tasks")]
-pub async fn get_tasks_for_user(pg_pool: web::Data<PgPool>, user_id: web::Path<i32>) -> impl Responder {
+pub async fn get_tasks_for_user(
+    pg_pool: web::Data<PgPool>,
+    user_id: web::Path<i32>,
+) -> impl Responder {
     info!("Get tasks for user {user_id}");
     let db_cxn = pg_pool.get_ref();
     let tasks = db::get_tasks_for_user(db_cxn, user_id.into_inner()).await;
@@ -85,25 +91,25 @@ pub struct GetTaskPath {
 }
 
 #[get("/users/{user_id}/tasks/{task_id}")]
-pub async fn get_task_for_user(pg_pool: web::Data<PgPool>, path: web::Path<GetTaskPath>) -> impl Responder {
+pub async fn get_task_for_user(
+    pg_pool: web::Data<PgPool>,
+    path: web::Path<GetTaskPath>,
+) -> impl Responder {
     info!("Get task {} for user {}", path.task_id, path.user_id);
     let db_cxn = pg_pool.get_ref();
     let task = db::get_task_for_user(db_cxn, path.user_id, path.task_id).await;
     match task {
         Ok(task) => HttpResponse::Ok().json(task),
-        Err(db_err) => {
-            
-            match db_err {
-                DbError::NoResults => {
-                    warn!("Task not found.");
-                    HttpResponse::NotFound().body("Task not found.")
-                },
-                _ => {
-                    error!("Task retrieve failure: {}", db_err);
-                    HttpResponse::InternalServerError().body("Failed to retrieve task")
-                },
+        Err(db_err) => match db_err {
+            DbError::NoResults => {
+                warn!("Task not found.");
+                HttpResponse::NotFound().body("Task not found.")
             }
-        }
+            _ => {
+                error!("Task retrieve failure: {}", db_err);
+                HttpResponse::InternalServerError().body("Failed to retrieve task")
+            }
+        },
     }
 }
 
@@ -113,24 +119,34 @@ struct InsertedTask {
 }
 
 #[post("/users/{user_id}/tasks")]
-pub async fn add_task_for_user(pg_pool: web::Data<PgPool>, user_id: web::Path<i32>, task_data: web::Json<db::NewTask>) -> impl Responder {
+pub async fn add_task_for_user(
+    pg_pool: web::Data<PgPool>,
+    user_id: web::Path<i32>,
+    task_data: web::Json<db::NewTask>,
+) -> impl Responder {
     info!("Adding task for user {user_id}");
     let db_cxn = pg_pool.get_ref();
-    let inserted_task = db::add_task_for_user(db_cxn, user_id.into_inner(), &task_data.into_inner()).await;
+    let inserted_task =
+        db::add_task_for_user(db_cxn, user_id.into_inner(), &task_data.into_inner()).await;
     match inserted_task {
         Ok(id) => HttpResponse::Created().json(InsertedTask { id }),
         Err(db_err) => {
             error!("Insert task failure: {db_err}");
             HttpResponse::InternalServerError().body("Failed to add task.")
-        },
+        }
     }
 }
 
 #[patch("/tasks/{task_id}")]
-pub async fn update_task_for_user(pg_pool: web::Data<PgPool>, task_id: web::Path<i32>, task_data: web::Json<db::UpdateTask>) -> impl Responder {
+pub async fn update_task_for_user(
+    pg_pool: web::Data<PgPool>,
+    task_id: web::Path<i32>,
+    task_data: web::Json<db::UpdateTask>,
+) -> impl Responder {
     info!("Updating task {task_id}");
     let db_cxn = pg_pool.get_ref();
-    let update_result = db::update_user_task(db_cxn, task_id.into_inner(), &task_data.into_inner()).await;
+    let update_result =
+        db::update_user_task(db_cxn, task_id.into_inner(), &task_data.into_inner()).await;
     match update_result {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(db_err) => {
@@ -141,7 +157,10 @@ pub async fn update_task_for_user(pg_pool: web::Data<PgPool>, task_id: web::Path
 }
 
 #[delete("/tasks/{task_id}")]
-pub async fn delete_task_for_user(pg_pool: web::Data<PgPool>, task_id: web::Path<i32>) -> impl Responder {
+pub async fn delete_task_for_user(
+    pg_pool: web::Data<PgPool>,
+    task_id: web::Path<i32>,
+) -> impl Responder {
     info!("Deleting task {task_id}");
     let db_cxn = pg_pool.get_ref();
     let delete_result = db::delete_user_task(db_cxn, task_id.into_inner()).await;
