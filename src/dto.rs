9b9cee9af375c92cb1ff398ea3e5ef251b6ed68f
Evan Rittenhouse <emanguy1@gmail.com>
2022-06-20 10:30:10 -0400

    Add sample test, testing config

--- src/dto.rs
+++ src/dto.rs
@@ -22,3 +22,23 @@ pub struct UpdateTask {
     #[validate(length(min = 1))]
     pub item_desc: String,
 }
+
+#[cfg(test)]
+mod dto_tests {
+    use super::*;
+
+    #[test]
+    fn bad_user_data_gets_rejected() {
+        let bad_user = NewUser {
+            first_name: (0..35).map(|_| "A").collect(),
+            last_name: (0..55).map(|_| "B").collect(),
+        };
+        let validation_result = bad_user.validate();
+        assert!(validation_result.is_err());
+        let validation_errors = validation_result.unwrap_err();
+        let field_validations = validation_errors.field_errors();
+        assert!(field_validations.contains_key("first_name"));
+        assert!(field_validations.contains_key("last_name"));
+        println!("Hellur");
+    }
+}
